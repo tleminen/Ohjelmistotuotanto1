9cef4c97d84f2212f41c26bf4006356293921f48
Olli Hilke <hilkolli@student.uef.fi>
2023-04-21 14:44:54 +0300

    Tehty Tietokantayhteys.java Tänne tehdään tietokannanyhteyden muodostaminen.

---
+++ app/src/main/java/com/oh/time4play/Tietokantayhteys.java
@@ -0,0 +1,22 @@
+package com.oh.time4play;
+
+
+/**
+ * Tietokantayhteyden muodostaminen
+ */
+public class Tietokantayhteys {
+    /**
+     * Tänne tehdään tietokantayhteyden muodostaminen
+     */
+    public static void yhdistaTietokantaan(String Kayttajatunnus, String Salasana) {
+
+    }
+
+    /**
+     * Tänne tehdään tietokantayhteyden katkaiseminen
+     */
+    public static void katkaiseYhteysTietokantaan() {
+
+
+    }
+}
--- app/src/main/java/com/oh/time4play/Toimip_hallinta_kyselyt.java
+++ app/src/main/java/com/oh/time4play/Toimip_hallinta_kyselyt.java
@@ -1,14 +1,28 @@
 package com.oh.time4play;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+//kesken
 public class Toimip_hallinta_kyselyt {
-
+/*
     //Aloitettu tekemään
-    void getAllToimipisteet() {
-       /* System.out.println("Lukee dataa..:");
-        connection.prepareStatement("""
-                SELECT paikka, nimi
+    Toimip_hallintaMuuttujat getAllToimipisteet(Connection tietokantayhteys) throws SQLException {
+        System.out.println("Lukee dataa..:");
+        try (PreparedStatement statement = tietokantayhteys.prepareStatement("""
+                SELECT Kaupunki, Nimi
                 FROM toimipiste
                 ORDER BY paikka
-                """) */
+                """)) {
+            ResultSet resultSet = statement.executeQuery();
+            Toimip_hallintaMuuttujat[] muuttujat = new Toimip_hallintaMuuttujat[resultSet.]{};
+            while (resultSet.next()) {
+                muuttujat.Paikkakunta = resultSet.getString("Kaupunki");
+                muuttujat.Nimi = resultSet.getString("Nimi");
+            }
+        }
+
     }
+    */
 }
