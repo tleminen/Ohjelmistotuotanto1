2f58de0d6e5ea5d87400c0e5ae698a37852802ba
Olli Hilke <hilkolli@student.uef.fi>
2023-04-21 13:37:11 +0300

    Muutettu kaikki layoutin tekstit käyttämään @string. Tehty Toimip_hallinta layout, aloitettu toimip_hallinnan recyclerin teko.

---
+++ app/src/main/java/com/oh/time4play/Toimip_hallintaItem.java
@@ -0,0 +1,8 @@
+package com.oh.time4play;
+
+import androidx.fragment.app.Fragment;
+
+//Luokan ohjelmointi valmis
+public class Toimip_hallintaItem extends Fragment {
+    public Toimip_hallintaItem() {super(R.layout.fragment_toimip_hallinta_item);}
+}
---
+++ app/src/main/java/com/oh/time4play/Toimip_hallintaListAdapter.java
@@ -0,0 +1,65 @@
+package com.oh.time4play;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.RecyclerView;
+//Ohjelmointi kesken
+/*
+public class Toimip_hallintaListAdapter extends RecyclerView.Adapter<Toimip_hallintaListAdapter>.ViewHolder {
+    Toimip_hallintaMuuttujat[] localDataset;
+    Bundle bundle = new Bundle();
+
+    public Toimip_hallintaListAdapter(Toimip_hallintaMuuttujat[] dataset) {localDataset=dataset;}
+
+    View.OnClickListener Toimip_hallintaListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int position = (int) v.getTag();
+            bundle.putString("Paikkakunta", String.valueOf(localDataset[position].Paikkakunta));
+            bundle.putString("Nimi"), String.valueOf(localDataset[position].Nimi);
+        }
+    };
+
+    @NonNull
+    @Override
+    public Toimip_hallintaListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_toimip_hallinta_item,parent,false);
+
+        return new ViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull Toimip_hallintaListAdapter.ViewHolder holder, int position) {
+        holder.itemView.setTag(position);
+        holder.itemView.setOnClickListener(Toimip_hallintaListener);
+
+        holder.textView.setText(localDataset[position].Paikkakunta);
+        holder.textView2.setText(localDataset[position].Nimi);
+    }
+
+    @Override
+    public int getItemCount() {return localDataset.length;}
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public final TextView textView;
+        public final TextView textView2;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            textView = (TextView) itemView.findViewById(R.id.twToimip_HallintaItemPaikka);
+            textView2 = (TextView) itemView.findViewById(R.id.twToimip_HallintaItemNimi);
+        }
+    }
+}
+*/
