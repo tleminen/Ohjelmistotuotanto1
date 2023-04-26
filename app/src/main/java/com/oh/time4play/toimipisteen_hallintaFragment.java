996ae46bde8e928c562e7a6fa878518a88c900ec
Olli Hilke <hilkolli@student.uef.fi>
2023-04-26 10:10:47 +0300

    toimipisteen_hallintaFragment aloitettu

--- app/src/main/java/com/oh/time4play/toimipisteen_hallintaFragment.java
+++ app/src/main/java/com/oh/time4play/toimipisteen_hallintaFragment.java
@@ -3,62 +3,46 @@ package com.oh.time4play;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link toimipisteen_hallintaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.oh.time4play.toimipisteen_hallintaFragmentDirections;
+
+
 public class toimipisteen_hallintaFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public toimipisteen_hallintaFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment toimipisteen_hallintaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static toimipisteen_hallintaFragment newInstance(String param1, String param2) {
-        toimipisteen_hallintaFragment fragment = new toimipisteen_hallintaFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_toimipisteen_hallinta, container, false);
+
+        String kayttajatunnus = toimip_poistoFragmentArgs.fromBundle(getArguments()).getKirjautunutKayttaja();
+        String salasana = toimip_poistoFragmentArgs.fromBundle(getArguments()).getKirjautunutSalasana();
+
+        Button thLisaaKentta = view.findViewById(R.id.bt_LisaaKentta_toimipisteen_hallinta);
+        Button thMuokkaaKentta = view.findViewById(R.id.bt_muokkaaKenttaa_toimipisteen_hallinta);
+        Button thUusiPelivaline = view.findViewById(R.id.bt_uusiPelivaline_toimipisteen_hallinta);
+        Button thMuokkaaPelivaline = view.findViewById(R.id.bt_muokkaaPelivalineita_toimipisteen_hallinta);
+
+        Button tpPoistu = view.findViewById(R.id.bt_poistu_toimipisteen_hallinta);
+
+        thLisaaKentta.setOnClickListener(e -> {
+            com.oh.time4play.toimipisteen_hallintaFragmentDirections.ActionToimipisteenHallintaFragmentToThKenttaLisaysFragment action = com.oh.time4play.toimipisteen_hallintaFragmentDirections.actionToimipisteenHallintaFragmentToThKenttaLisaysFragment(kayttajatunnus,salasana);
+            Navigation.findNavController(view).navigate(action);
+        });
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_toimipisteen_hallinta, container, false);
+        return view;
     }
 }
