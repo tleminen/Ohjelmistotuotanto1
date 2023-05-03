e3e665fab133257a74eae221bea938aefae6ccac
Olli Hilke <hilkolli@student.uef.fi>
2023-05-03 21:49:33 +0300

    Raportointi toimii nyt

--- app/src/main/java/com/oh/time4play/Raportti_kyselyt.java
+++ app/src/main/java/com/oh/time4play/Raportti_kyselyt.java
@@ -9,14 +9,15 @@ import java.util.ArrayList;
 public class Raportti_kyselyt {
 
     public static String getLocalVarausraportti(Connection connection, String kayttajatunnus, String alkupvm, String loppupvm) throws SQLException {
-        String raportti = "";
+        String raportti = "ID\t|\tPVM\t|\tAlkamisaika\t|\tKenttä\t|\tAsiakkaan email\n";
         System.out.println("Luodaan varausraportti...");
         try (PreparedStatement statement = connection.prepareStatement("""
-                SELECT VarausID, VarauksenPVM, VarauksenAika, varaus.KenttaID, email
+                SELECT VarausID, VarauksenPVM, VarauksenAika, kentat.Kenttanimi, email
                 FROM varaus, toimipiste, kentat
                 WHERE toimipiste.Toimipistevastaava LIKE ?
                 AND VarauksenPVM BETWEEN ? AND ?
-                AND varaus.KenttaID = kentat.KenttaID AND kentat.Toimipistevastaava = toimipiste.Toimipistevastaava 
+                AND varaus.KenttaID = kentat.KenttaID AND kentat.Toimipistevastaava = toimipiste.Toimipistevastaava
+                ORDER BY VarauksenPVM, VarauksenAika
                 """)) {
             statement.setString(1,kayttajatunnus);
             statement.setString(2,alkupvm);
@@ -25,7 +26,7 @@ public class Raportti_kyselyt {
             System.out.println("Varausrapostti luotu");
             while (resultSet.next()) {
                 System.out.println("Oli resultsetissä raporttia");
-                raportti += resultSet.getInt("VarausID") + "\t" + resultSet.getString("VarauksenPVM") + "\t" + resultSet.getString("VarauksenAika") + "\t" + resultSet.getString("varaus.KenttaID") + "\t" + resultSet.getString("email") + "\n";
+                raportti += resultSet.getInt("VarausID") + "\t|\t" + resultSet.getString("VarauksenPVM") + "\t|\t" + resultSet.getString("VarauksenAika") + ":00" + "\t|\t" + resultSet.getString("kentat.Kenttanimi") + "\t|\t" + resultSet.getString("email") + "\n";
             }
             return raportti;
 
