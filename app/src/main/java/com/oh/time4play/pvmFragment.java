ac560e2505c33058a9ef27068272437ccf268a55
tleminen <tleminen@student.uef.fi>
2023-05-05 14:01:01 +0300

    pvmFragment ohjelmoitu valmiiksi

--- app/src/main/java/com/oh/time4play/pvmFragment.java
+++ app/src/main/java/com/oh/time4play/pvmFragment.java
@@ -3,62 +3,52 @@ package com.oh.time4play;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link pvmFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class pvmFragment extends Fragment {
+    public String valittupvm;
+    public String valittulaji;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    public pvmFragment(){
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public pvmFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment pvmFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static pvmFragment newInstance(String param1, String param2) {
-        pvmFragment fragment = new pvmFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pvm, container, false);
+        View view = inflater.inflate(R.layout.fragment_pvm, container, false);
+
+        String kayttajatunnus = pvmFragmentArgs.fromBundle(getArguments()).getKirjautunutKayttaja();
+        String salasana = pvmFragmentArgs.fromBundle(getArguments()).getKirjautunutSalasana();
+        String valittuToimipiste = pvmFragmentArgs.fromBundle(getArguments()).getValittuToimipiste();
+
+        DatePicker Alkupvm = view.findViewById(R.id.cv_PVM);
+        RadioButton TennisNappula = view.findViewById(R.id.rb_PVM_Tennis);
+        RadioButton SulkapalloNappula = view.findViewById(R.id.rb_PVM_Sulkapallo);
+        Button Vahvista = view.findViewById(R.id.bt_PVM_VahvistaNappi);
+
+        Vahvista.setOnClickListener(e->{
+            valittupvm = String.valueOf(Alkupvm.getYear()+"-"+ Alkupvm.getMonth()+"-"+ Alkupvm.getDayOfMonth());
+            if (TennisNappula.isSelected() | SulkapalloNappula.isSelected()) {
+                if (TennisNappula.isSelected()){
+                    valittulaji = String.valueOf(TennisNappula);
+                } else if (SulkapalloNappula.isSelected()){
+                    valittulaji = String.valueOf(SulkapalloNappula);
+                }
+            }
+            pvmFragmentDirections.ActionPvmFragmentToKenttaFragment action = pvmFragmentDirections.actionPvmFragmentToKenttaFragment(kayttajatunnus,salasana,valittuToimipiste,valittupvm, valittulaji);
+            Navigation.findNavController(view).navigate(action);
+        });
+        return view;
     }
 }
